d3c028af9684cf0a9580d8c7a5f2f11fd932d8d8
22 <60903333+nini22P@users.noreply.github.com>
2025-07-19 09:03:01 +0800

    fix image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div
      className='pl-4 pt-[60dvh] pr-4 pb-4 lg:pl-8 lg:pt-8 lg:pb-8 lg:pr-[40%] xl:pr-[45%]'
-     style={{ background: 'url(./main_vizual.webp) no-repeat fixed top right / cover' }}
+     style={{ background: 'url(./main_visual.webp) no-repeat fixed top right / cover' }}
     >
       <main className='p-8 bg-[#FFFFFFcc] overflow-hidden rounded border-2 border-red-300 shadow-lg backdrop-blur-xs'>
         <div className="prose max-w-fit prose-hr:my-8 prose-ul:m-0 prose-li:m-0 prose-a:no-underline prose-a:hover:underline prose-a:text-red-500">
