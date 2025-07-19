e5f31ec4610756467edeeeb4bbe7162c293f67b6
22 <60903333+nini22P@users.noreply.github.com>
2025-07-19 09:00:48 +0800

    add main_visual and update doc

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import Moyu from '../markdown/moyu.md';
 
 export default function Home() {
   return (
-    <div className='max-w-3xl m-auto my-8 p-4'>
-      <div className="prose dark:prose-invert max-w-fit prose-hr:my-8 prose-ul:m-0 prose-li:m-0 prose-a:no-underline prose-a:hover:underline prose-a:text-red-500">
-        <Moyu />
-      </div>
+    <div
+     className='pl-4 pt-[60dvh] pr-4 pb-4 lg:pl-8 lg:pt-8 lg:pb-8 lg:pr-[40%] xl:pr-[45%]'
+     style={{ background: 'url(./main_vizual.webp) no-repeat fixed top right / cover' }}
+    >
+      <main className='p-8 bg-[#FFFFFFcc] overflow-hidden rounded border-2 border-red-300 shadow-lg backdrop-blur-xs'>
+        <div className="prose max-w-fit prose-hr:my-8 prose-ul:m-0 prose-li:m-0 prose-a:no-underline prose-a:hover:underline prose-a:text-red-500">
+          <Moyu />
+        </div>
+      </main>
     </div>
   );
 }
