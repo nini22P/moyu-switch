34f06bea785aa9432309d87d8f8a1e690a1bc3d9
22 <60903333+nini22P@users.noreply.github.com>
2025-07-19 10:48:27 +0800

    move doc

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Moyu from '../markdown/moyu.md';
+import README from '../README.md';
 
 export default function Home() {
   return (
@@ -6,9 +6,9 @@ export default function Home() {
      className='pl-4 pt-[60dvh] pr-4 pb-4 lg:pl-8 lg:pt-8 lg:pb-8 lg:pr-[40%] xl:pr-[45%]'
      style={{ background: 'url(./main_visual.webp) no-repeat fixed top right / cover' }}
     >
-      <main className='p-8 bg-[#FFFFFFcc] overflow-hidden rounded border-2 border-red-300 shadow-lg backdrop-blur-xs'>
+      <main className='p-8 bg-[#FFFFFFcc] overflow-hidden rounded border-2 border-red-300 shadow-2xl backdrop-blur-xs'>
         <div className="prose max-w-fit prose-hr:my-8 prose-ul:m-0 prose-li:m-0 prose-a:no-underline prose-a:hover:underline prose-a:text-red-500">
-          <Moyu />
+          <README />
         </div>
       </main>
     </div>
